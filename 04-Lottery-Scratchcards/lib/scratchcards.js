3836773f7fcd6b472289a13efc2833db5039cf56
jose tomas ruiz <j_tomas_93@hotmail.com>
2024-06-02 22:17:29 -0400

    semi completo crashcards

--- 04-Lottery-Scratchcards/lib/scratchcards.js
+++ 04-Lottery-Scratchcards/lib/scratchcards.js
@@ -1,1 +1,9 @@
 // TODO: Write your gambling application here.
+const cards = document.querySelectorAll("li.scratchcard")
+
+cards.forEach((card) =>{
+  card.addEventListener("click",() => {
+    card.dataset.scratched = "true";
+    card.innerText = card.dataset.amount;
+  })
+})
