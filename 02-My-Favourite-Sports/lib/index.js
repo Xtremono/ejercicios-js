4041a44e240038f07f5bb9c8f430a2e954ccf1a9
jose tomas ruiz <j_tomas_93@hotmail.com>
2024-06-02 20:20:55 -0400

    favorite sport completado

--- 02-My-Favourite-Sports/lib/index.js
+++ 02-My-Favourite-Sports/lib/index.js
@@ -1,1 +1,8 @@
 // TODO: write your code here!
+const buttons = document.querySelectorAll(".clickable");
+
+buttons.forEach((button) =>{
+  button.addEventListener("click", () =>{
+   button.classList.toggle("active");
+  })
+})
